6c28fb63814513040b39e84ad999a48a26890ad1
Taopik Hidayat <taopikcoding17@gmail.com>
2026-04-07 08:42:24 +0700

    init workflow ci

---
+++ MLProject/modelling.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import mlflow
+import mlflow.sklearn
+
+# Load dataset
+df = pd.read_csv("namadataset_preprocessing/processed.csv")
+
+X = df.drop("Survived", axis=1)
+y = df["Survived"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    print(f"Accuracy: {acc}")
